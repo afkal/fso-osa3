ada2a2171bb7ff2f98ae086bbf08b9483e7fb954
Timo Laitila <timo.t.laitila@gmail.com>
2021-05-03 11:45:12 +0300

    Tehtävä 3.4

--- index.js
+++ index.js
@@ -55,6 +55,14 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
