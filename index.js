e13a6fd86b695f4e5c7e9669dcc5d81d6be892fe
Timo Laitila <timo.t.laitila@gmail.com>
2021-05-03 14:13:05 +0300

    Tehtävä 3.7

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+// setup the logger
+app.use(morgan('tiny'))
 
 let persons = [
     {
