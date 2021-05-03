e56fa84e3adaee49fed37d7c20f3fb36333850e6
Timo Laitila <timo.t.laitila@gmail.com>
2021-05-03 23:25:45 +0300

    Tehtävä 3.10

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+
 
 // Define token
 morgan.token('type', function (req, res) {
