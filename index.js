d6ff5446cbc12ca9edf4a867251407b50bf493bd
Timo Laitila <timo.t.laitila@gmail.com>
2021-05-07 18:00:22 +0300

    Tehtävä 3.14

--- index.js
+++ index.js
@@ -16,35 +16,7 @@ morgan.token('type', function (req, res) {
 
 // setup the logger
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
-/*
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
-*/
-/*
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-*/
+
 app.get('/info', (req, res) => {
   let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'long'};
   options.timeZone = 'EET';
